9ff2cf1e16899ac3e0a0b69f653f114978bcafff
jeanreyshorts <jrey8891@gmail.com>
2025-07-09 03:53:39 +0200

    commit-1-09/07

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
+  //Variables, datos
+  mensaje="Hola desde el home page.ts"
   constructor() {}
+
+  //funciones
+
+  saludar(){
+    console.log(this.mensaje);
+
+  }
 }
